255c63b88e7d40750d4ac08f5248ca799a77ac08
RhythmusByte <akhilmahesh@proton.me>
2024-11-18 17:08:14 +0530

    Adjusted Icon Size

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         <h2 className="name">Akhil Mahesh</h2>
         <p className="about-1">Software Engineer Aspirant</p>
         <p className="about-2">
-          Student at <b>University of Calicut</b>
+          Student at University of Calicut
         </p>
 
         {/* Social Links */}
