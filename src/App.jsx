d7afebcfea6955fa0160e6a9e4d1026a975e2edb
RhythmusByte <akhilmahesh@proton.me>
2024-11-20 14:56:19 +0530

    Fixed error on email icon

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,14 @@ import './App.css';
 function App() {
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
-  useEffect(() => {
-    const copyBtns = document.querySelectorAll('.copy');
-
-    async function copyText(e) {
-      e.preventDefault();
-      const link = e.target.getAttribute('link');
-      console.log(`The link: "${link}"`);
-      try {
-        await navigator.clipboard.writeText(link);
-        alert('Link copied to clipboard');
-      } catch (err) {
-        console.error('Failed to copy text:', err);
-      }
+  const handleCopy = async (link) => {
+    try {
+      await navigator.clipboard.writeText(link);
+      alert('Link copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy text:', err);
     }
-
-    copyBtns.forEach((btn) => btn.addEventListener('click', copyText));
-
-    // Cleanup event listeners on unmount
-    return () => {
-      copyBtns.forEach((btn) => btn.removeEventListener('click', copyText));
-    };
-  }, []);
+  };
 
   return (
     <>
@@ -46,26 +32,32 @@ function App() {
 
         {/* Social Links */}
         {[
-          { name: 'LinkedIn', link: 'https://www.linkedin.com/in/rhythmusbyte', icon: 'fa-linkedin' },
-          { name: 'GitHub', link: 'https://github.com/RhythmusByte', icon: 'fa-github' },
-          { name: 'Instagram', link: 'https://instagram.com/rhythmusbyte', icon: 'fa-instagram' },
-          { name: 'Email', link: 'mailto:akhilmahesh@proton.me', icon: 'fa-envelope' },
-          { name: 'Codepen', link: 'https://codepen.io/RhythmusByte', icon: 'fa-codepen' },
-          { name: 'X (Twitter)', link: 'https://x.com/RhythmusByte', icon: 'fa-x-twitter' },
-          { name: 'Discord', link: 'https://discordapp.com/users/1066242357793656842', icon: 'fa-discord' },
-          { name: 'Facebook', link: 'https://www.facebook.com/RhythmusByte', icon: 'fa-facebook' },
-          { name: 'Threads', link: 'https://www.threads.net/@rhythmusbyte', icon: 'fa-threads' },
-          { name: 'Medium', link: 'https://medium.com/@rhythmusbyte', icon: 'fa-medium' },
-          { name: 'Dev', link: 'https://dev.to/rhythmusbyte', icon: 'fa-dev' },
-          { name: 'Blog', link: 'https://rhythmusbyte.blogspot.com', icon: 'fa-blogger' },
-        ].map(({ name, link, icon }) => (
+          { name: 'LinkedIn', link: 'https://www.linkedin.com/in/rhythmusbyte', icon: 'fa-linkedin', isBrand: true },
+          { name: 'GitHub', link: 'https://github.com/RhythmusByte', icon: 'fa-github', isBrand: true },
+          { name: 'Instagram', link: 'https://instagram.com/rhythmusbyte', icon: 'fa-instagram', isBrand: true },
+          { name: 'Email', link: 'mailto:akhilmahesh@proton.me', icon: 'fa-envelope', isBrand: false },
+          { name: 'Codepen', link: 'https://codepen.io/RhythmusByte', icon: 'fa-codepen', isBrand: true },
+          { name: 'X (Twitter)', link: 'https://x.com/RhythmusByte', icon: 'fa-x-twitter', isBrand: true },
+          { name: 'Discord', link: 'https://discordapp.com/users/1066242357793656842', icon: 'fa-discord', isBrand: true },
+          { name: 'Facebook', link: 'https://www.facebook.com/RhythmusByte', icon: 'fa-facebook', isBrand: true },
+          { name: 'Threads', link: 'https://www.threads.net/@rhythmusbyte', icon: 'fa-threads', isBrand: true },
+          { name: 'Medium', link: 'https://medium.com/@rhythmusbyte', icon: 'fa-medium', isBrand: true },
+          { name: 'Dev', link: 'https://dev.to/rhythmusbyte', icon: 'fa-dev', isBrand: true },
+          { name: 'Blog', link: 'https://rhythmusbyte.blogspot.com', icon: 'fa-blogger', isBrand: true },
+        ].map(({ name, link, icon, isBrand }) => (
           <div className="tiles" key={name}>
             <a href={link} className="link">
               <div className="icon">
-                <i className={`fa-brands ${icon}`}></i>
+                <i className={`${isBrand ? 'fa-brands' : 'fa-solid'} ${icon}`}></i>
               </div>
               <p className="social">{name}</p>
-              <div className="copy" link={link}>
+              <div 
+                className="copy" 
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleCopy(link);
+                }}
+              >
                 <i className="fa-brands fa-creative-commons-share"></i>
               </div>
             </a>
@@ -83,4 +75,3 @@ function App() {
 }
 
 export default App;
-
